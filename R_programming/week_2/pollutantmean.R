511a3cbc0288c378ebb5b03bacc2688adce1ac8a
chennipman <chennipman@hotmail.com>
2015-03-16 00:27:39 +0100

    Working version of pollutantmean

--- R_programming/week_2/pollutantmean.R
+++ R_programming/week_2/pollutantmean.R
@@ -12,32 +12,25 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## Return the mean of the pollutant across all monitors list
   ## in the 'id' vector (ignoring NA values)
 
+  all_pol <- vector(mode = "numeric")
 
-  # Select the column in the data file
-  if (pollutant=="sulfate") {
-    column <- 1
-  } else if (pollutant=="nitrate") {
-    column <- 2
-  } else {
-    print("Error: Unknown pollutant in pollutantmean")
-    return
-  }
-      
-  print(column)
-  
   # for loop over all the files (all id's)
+  for(i in id) {
+    # make filenumber a string with leading zero's 
+    i_format <- formatC(i, width = 3, flag = "0")
 
+    # create filepath
+    filepath = paste(directory,"/",i_format,".csv", sep="")
+    
+    # read csv file
+    frame <- read.csv(filepath)
 
-  # Ignore all the NA values
-
-  # calculate the mean
- 
-  pollutantmean <- 1
+    # add non NA pollutant data to the vector
+    only_pol <- frame[[pollutant]]
+    pol <- only_pol[!is.na(only_pol)]
+    all_pol <- c(all_pol,pol)
+  }
+  
+  pollutantmean <- mean(all_pol)
 }
-
-
-
-
-
-
 
